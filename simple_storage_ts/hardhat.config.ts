f4269450ef913826e3ecb4030e4167da1e52cf27
LaupWing <loc_strict@hotmail.com>
2022-10-26 14:06:16 +0200

    yeah

--- simple_storage_ts/hardhat.config.ts
+++ simple_storage_ts/hardhat.config.ts
@@ -1,8 +1,11 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-waffle"
+import "@typechain/hardhat"
+import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+   solidity: "0.8.17",
+}
 
-export default config;
+export default config
