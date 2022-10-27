94fd218dddc7e9fa8e0654a1d4d7270a8216a32b
LaupWing <loc_strict@hotmail.com>
2022-10-27 14:32:05 +0200

    filing in dotenv

--- fund_me/hardhat.config.js
+++ fund_me/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+   defaultNetwork: "hardhat",
+   networks:{
+      hardhat:{
+         chainId: 31337
+      },
+      goerli:{
+         
+      }
+   },
+   solidity: "0.8.17",
 };
