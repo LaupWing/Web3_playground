40a7788e2841c688dbc16c38034c6f9e4de33578
LaupWing <loc_strict@hotmail.com>
2022-10-27 14:16:00 +0200

    initial values added

--- fund_me/contracts/Lock.sol
+++ fund_me/contracts/Lock.sol
@@ -1,34 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract Fundme {
+   
+   uint256 public constant MINIMUM_USD = 50 * 10**18;
+    address private immutable i_owner;
+   address[] private s_funders;
+   mapping(address => uint256) private s_addressToAmountFunded;
+   AggregatorV3Interface private s_pricefeed;
 
-    event Withdrawal(uint amount, uint when);
+   constructor(address priceFeedAddress){
+      s_pricefeed = AggregatorV3Interface(priceFeedAddress);
+      i_owner = msg.sender;
+   }
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
+   // function fund() public payable{
+      
+   // }
 }
