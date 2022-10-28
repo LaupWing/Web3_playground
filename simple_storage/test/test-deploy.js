31b840aab84a721904c6e00b02148f189bd6ed37
LaupWing <loc_strict@hotmail.com>
2022-10-28 14:20:40 +0200

    test:Should start with a favorite number of 69

--- simple_storage/test/test-deploy.js
+++ simple_storage/test/test-deploy.js
@@ -1,11 +1,14 @@
 const { getNamedAccounts, ethers } = require("hardhat")
-
+const { expect } = require("chai")
 
 describe("SimpleStorage", function () {
 
    let deployer
    let simpleStorage
    let simpleStorageFactory
+
+   const startingValue = "69"
+
    beforeEach(async () => {
       simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
       deployer = (await getNamedAccounts()).deployer
@@ -13,7 +16,11 @@ describe("SimpleStorage", function () {
       simpleStorage = await ethers.getContract("SimpleStorage", deployer)
    })
 
-   it("deployed contract correctly", async () => {
-      console.log(simpleStorage)
+   // it("deployed contract correctly", async () => {
+   //    console.log(simpleStorage)
+   // })
+   it("Should start with a favorite number of 69", async () => {
+      const currentValue = await simpleStorage.retrieve()
+      expect(currentValue.toString()).to.equal(startingValue)
    })
 })
