294c07cdd5a13ea298ff18eaa3f75826f76084b3
LaupWing <loc_strict@hotmail.com>
2022-10-28 14:59:08 +0200

    mocks can be sended

--- fund_me/deploy/00-deploy-mocks.js
+++ fund_me/deploy/00-deploy-mocks.js
@@ -1,3 +1,5 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 
 const DECIMALS = "8"
 const INITIAL_PRICE = "200000000000"
@@ -6,9 +8,19 @@ const INITIAL_PRICE = "200000000000"
 module.exports = async ({ getNamedAccounts, deployments }) => {
    const { log, deploy } = deployments
    const { deployer } = await getNamedAccounts()
-   const test = await getNamedAccounts()
+   const chainId = network.config.chainId
 
-   console.log(deployer)
-   console.log(test)
-   console.log(deploy)
+   if(developmentChains.includes(network.name)){
+      log("Local network detected! Deploying mocks...")  
+      await deploy("MockV3Aggregator", {
+         from: deployer,
+         log: true,
+         args: [DECIMALS, INITIAL_PRICE]
+      })
+      log("Mocks deployed")
+      log("----------------------------------------------------------")
+      log("You are deploying to a local network, you'll need a local nethwork to interact")
+      log("Pleade run `npx hardhat console` to interact witht deployed smart contract")
+      log("----------------------------------------------------------")
+   }
 }
