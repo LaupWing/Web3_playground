844cdfa021f971e05d41e3dc1d0c7cd7675c5a5d
LaupWing <loc_strict@hotmail.com>
2022-10-29 14:11:53 +0200

    wait

--- fund_me/deploy/01-deploy-fundme.js
+++ fund_me/deploy/01-deploy-fundme.js
@@ -1,5 +1,33 @@
+const { network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
 
-module.exports = async( {getNamedAccounts, deployments}) =>{
-   
+module.exports = async ({ getNamedAccounts, deployments }) => {
+   const { deploy, log } = deployments
+   const { deployer } = await getNamedAccounts()
+   const isDevelopmentChain = developmentChains.includes(network.name)
+
+   let ethUsdPricefeedAddress
+   if (isDevelopmentChain) {
+      const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+      ethUsdPricefeedAddress = ethUsdAggregator.address
+   } else { 
+      ethUsdPricefeedAddress = networkConfig[network.config.chainId]["ethUsdPriceFeed"]
+   }
+   log("-------------------------------------------------------")
+   log("Deploying FundMe and waiting for confirmations...")
+   const fundMe = await deploy("FundMe", {
+      from: deployer,
+      args: [ethUsdPricefeedAddress],
+      log: true,
+      waitConfirmations: network.config.blockConfirmations || 1
+   })
+
+   log(`FundMe deployed at ${fundMe.address}`)
+   if(
+      !isDevelopmentChain &&
+      process.env.ETHERSCAN_API_KEY
+   ){
+      veri
+   }
 }
