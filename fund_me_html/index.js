d092eba3b06a75523ea63a6be35a231eac77cc8f
LaupWing <loc_strict@hotmail.com>
2022-11-01 15:40:15 +0100

    update

--- fund_me_html/index.js
+++ fund_me_html/index.js
@@ -6,6 +6,7 @@ document.getElementById("connectButton").addEventListener("click", connect)
 document.getElementById("fundButton").addEventListener("click", fund)
 const withdrawButton = document.getElementById("withdrawButton")
 const balanceButton = document.getElementById("balanceButton")
+balanceButton.addEventListener("click", getBalance)
 
 
 
@@ -25,17 +26,46 @@ async function connect() {
 async function fund(){
    const ethAmount = document.getElementById("ethAmount").value
    console.log(ethAmount)
-   // if(typeof window.ethereum !== "undefined"){
-   //    const provider = new ethers.providers.Web3Provider(window.ethereum)
-   //    const signer = provider.getSigner()
-   //    const contract = new ethers.Contract(contractAddress, abi, signer)
-   //    try{
-   //       const transactionResponse = await contract.fund({
-   //          value: ethers.utils.parseEther(ethAmount)
-   //       })
+   if(typeof window.ethereum !== "undefined"){
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(contractAddress, abi, signer)
+      try{
+         const transactionResponse = await contract.fund({
+            value: ethers.utils.parseEther(ethAmount)
+         })
+         await listenForTransactionMine(transactionResponse, provider)
+      }catch(e){
+         console.error(e)
+      }
+   }
+}
 
-   //    }catch(e){
-   //       console.error(e)
-   //    }
-   // }
+async function getBalance(){
+   if(typeof window.ethereum != "undefined"){
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      try{
+         const balance = await provider.getBalance(contractAddress)
+         console.log(balance)
+         console.log(ethers.utils.formatEther(balance))
+      }catch(e){
+         console.error(e)
+      }
+   }else{
+      balanceButton.innerHTML = "Please install MetaMask"
+   }
+}
+
+function listenForTransactionMine(transactionResponse, provider){
+   console.log(`Mining ${transactionResponse.hash}`)
+   return new Promise((resolve, reject)=>{
+      try{
+         provider.once(transactionResponse.hash, (transactionReceipt)=>{
+            console.log(`Completed with ${transactionReceipt.confirmations} confirmations`)
+         })
+         resolve()
+      }catch(e){
+         reject(e)
+      }
+   })
 }
