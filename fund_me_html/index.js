64678f9d120c13dcf64befaf7818286060189a13
LaupWing <loc_strict@hotmail.com>
2022-11-01 15:33:11 +0100

    udpate

--- fund_me_html/index.js
+++ fund_me_html/index.js
@@ -1,9 +1,10 @@
+import { abi, contractAddress } from "./constants.js"
 import { ethers } from "./ethers-5.2.esm.min.js"
 
 
-(document.getElementById("connectButton")).addEventListener("click", connect)
+document.getElementById("connectButton").addEventListener("click", connect)
+document.getElementById("fundButton").addEventListener("click", fund)
 const withdrawButton = document.getElementById("withdrawButton")
-const fundButton = document.getElementById("fundButton")
 const balanceButton = document.getElementById("balanceButton")
 
 
@@ -23,9 +24,18 @@ async function connect() {
 
 async function fund(){
    const ethAmount = document.getElementById("ethAmount").value
-   
-   if(typeof window.ethereum !== "undefined"){
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-   }
+   console.log(ethAmount)
+   // if(typeof window.ethereum !== "undefined"){
+   //    const provider = new ethers.providers.Web3Provider(window.ethereum)
+   //    const signer = provider.getSigner()
+   //    const contract = new ethers.Contract(contractAddress, abi, signer)
+   //    try{
+   //       const transactionResponse = await contract.fund({
+   //          value: ethers.utils.parseEther(ethAmount)
+   //       })
+
+   //    }catch(e){
+   //       console.error(e)
+   //    }
+   // }
 }
