ed971313e1671dca46273a03026ef95d2d6cc68d
LaupWing <loc_strict@hotmail.com>
2022-11-03 16:47:53 +0100

    lottttttery

--- lottery_smartcontract/deploy/01-deploy-lottery.js
+++ lottery_smartcontract/deploy/01-deploy-lottery.js
@@ -1,4 +1,6 @@
 const { network, ethers } = require("hardhat")
+const { networkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 const FUND_AMOUNT = ethers.utils.parseEther("1")
 
@@ -17,6 +19,36 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
       await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
    } else {
-      vrfCoordinatorV2Address
+      vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+      subscriptionId = networkConfig[chainId]["subscriptionId"]
    }
+
+   const waitBlockConfirmations = developmentChains.includes(network.name) ? 1 : VERIFICATION_BLOCK_CONFIRMATIONS
+   log("--------------------------------------------------------------------")
+   const args = [
+      vrfCoordinatorV2Address,
+      subscriptionId,
+      networkConfig[chainId]["gasLane"],
+      networkConfig[chainId]["keepersUpdateInterval"],
+      networkConfig[chainId]["lotteryEntranceFee"],
+      networkConfig[chainId]["callbackGasLimit"]
+   ]
+   const lottery = await deploy("Lottery", {
+      deployer,
+      args,
+      log: true,
+      waitConfirmations: waitBlockConfirmations
+   })
+
+   if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+      log("Verifying...")
+      await verify(lottery.address, args)
+   }
+
+   log("Enter lottery command:")
+   const networkName = network.name == "hardhat" ? "localhost" : network.name
+   log(`npx hardhat run scripts/enterLottery.js --network ${networkName}`)
+   log("------------------------------------------------------------")
 }
+
+module.exports.tags = ["all", "lottery"]
