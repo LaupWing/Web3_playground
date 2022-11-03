b5b0fbec22ba898260e1fd71a8e7b0b35f9c1773
LaupWing <loc_strict@hotmail.com>
2022-11-03 17:48:15 +0100

    udpate

---
+++ lottery_smartcontract/test/unit/Lottery.test.js
@@ -0,0 +1,29 @@
+const { expect } = require("chai");
+const { network, ethers, deployments } = require("hardhat");
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config");
+
+
+!developmentChains.includes(network.name)
+   ? describe.skip
+   : describe("Lottery Unit Tests", function () {
+      let lottery, lotteryContract, vrfCoordinatorV2Mock, lotteryEntranceFee, interval, player
+
+      beforeEach(async () => {
+         accounts = await ethers.getSigners()
+         player = accounts[1]
+         await deployments.fixture(["mocks", "lottery"])
+         vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+         lotteryContract = await ethers.getContract("Lottery")
+         lottery = lotteryContract.connect(player)
+         lotteryEntranceFee = await lottery.getEntranceFee()
+         interval = await lottery.getInterval()
+      })
+
+      describe("constructor", function () {
+         it("initializes the lottery correctly", async () => {
+            const lotteryState = (await lottery.getLotteryState()).toString()
+            expect(lotteryState).to.equal("0")
+            expect(interval.toString(), networkConfig[network.config.chainId]["keepersUpdateInterval"])
+         })
+      })
+   })
