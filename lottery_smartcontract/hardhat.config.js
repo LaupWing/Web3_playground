10ea8a271d03d09e5f03c3f1a53e4679626a75f9
LaupWing <loc_strict@hotmail.com>
2022-11-03 16:19:31 +0100

    networks

--- lottery_smartcontract/hardhat.config.js
+++ lottery_smartcontract/hardhat.config.js
@@ -6,6 +6,7 @@ require("dotenv").config()
 const MAIN_RPC_URL = process.env.MAIN_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
 const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0"
 const MNEMONIC = process.env.MNEMONIC || ""
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
@@ -16,4 +17,39 @@ const REPORT_GAS = process.env.REPORT_GAS || false
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
    solidity: "0.8.17",
+   networks:{
+      hardhat:{
+         chainId: 31337
+      },
+      localhost:{
+         chainId: 31337
+      },
+      goerli:{
+         url: GOERLI_RPC_URL,
+         accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+         // accounts:{
+         //    mnemonic: MNEMONIC
+         // },
+         savedDeployments: true,
+         chainId: 5
+      },
+      mainnet:{
+         url: MAIN_RPC_URL,
+         accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+         // accounts:{
+         //    mnemonic: MNEMONIC
+         // },
+         savedDeployments: true,
+         chainId: 1
+      },
+      polyogon:{
+         url: POLYGON_MAINNET_RPC_URL,
+         accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+         savedDeployments: true,
+         chainId: 137
+      },
+      localhost:{
+         chainId: 31337
+      },
+   }
 }
