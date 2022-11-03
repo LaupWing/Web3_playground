cd4301db127e53b3e6ec6b88721d5b3f570699aa
LaupWing <loc_strict@hotmail.com>
2022-11-03 16:30:20 +0100

    checking

---
+++ lottery_smartcontract/deploy/01-deploy-lottery.js
@@ -0,0 +1,20 @@
+const { network, ethers } = require("hardhat")
+
+const FUND_AMOUNT = ethers.utils.parseEther("1")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+   const { deploy, log } = deployments
+   const { deployer } = await getNamedAccounts()
+   const chainId = network.config.chainId
+   let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
+
+   if (chainId === 31337) {
+      vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+      vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+      const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+      const transactionReceipt = await transactionResponse.wait()
+      subscriptionId = transactionReceipt.events[0].args.subId
+
+      await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+   }
+}
