f53ea1321fc0389abfb34902d9db5c42be25f489
LaupWing <loc_strict@hotmail.com>
2022-11-03 16:50:25 +0100

    ait

--- lottery_smartcontract/hardhat.config.js
+++ lottery_smartcontract/hardhat.config.js
@@ -3,11 +3,11 @@ require("hardhat-deploy")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-const MAIN_RPC_URL = process.env.MAIN_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
+// const MAIN_RPC_URL = process.env.MAIN_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
-const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL || ""
+// const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0"
-const MNEMONIC = process.env.MNEMONIC || ""
+// const MNEMONIC = process.env.MNEMONIC || ""
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 const POLYGONSCN_API_KEY = process.env.POLYGONSCN_API_KEY || ""
@@ -16,15 +16,14 @@ const REPORT_GAS = process.env.REPORT_GAS || false
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-   solidity: "0.8.17",
-   networks:{
-      hardhat:{
+   networks: {
+      hardhat: {
          chainId: 31337
       },
-      localhost:{
+      localhost: {
          chainId: 31337
       },
-      goerli:{
+      goerli: {
          url: GOERLI_RPC_URL,
          accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
          // accounts:{
@@ -33,62 +32,62 @@ module.exports = {
          savedDeployments: true,
          chainId: 5
       },
-      mainnet:{
-         url: MAIN_RPC_URL,
-         accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-         // accounts:{
-         //    mnemonic: MNEMONIC
-         // },
-         savedDeployments: true,
-         chainId: 1
-      },
-      polyogon:{
-         url: POLYGON_MAINNET_RPC_URL,
-         accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-         savedDeployments: true,
-         chainId: 137
-      },
-      localhost:{
+      // mainnet:{
+      //    url: MAIN_RPC_URL,
+      //    accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //    // accounts:{
+      //    //    mnemonic: MNEMONIC
+      //    // },
+      //    savedDeployments: true,
+      //    chainId: 1
+      // },
+      // polyogon:{
+      //    url: POLYGON_MAINNET_RPC_URL,
+      //    accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //    savedDeployments: true,
+      //    chainId: 137
+      // },
+      localhost: {
          chainId: 31337
       },
    },
-   etherscan:{
-      apiKey:{
+   etherscan: {
+      apiKey: {
          goerli: ETHERSCAN_API_KEY,
          polyogon: POLYGONSCN_API_KEY
       }
    },
-   gasReporter:{
+   gasReporter: {
       enabled: REPORT_GAS,
       currency: "USD",
       outputFile: "gas-report.txt",
       noColors: true,
       // coinmarketcap: process.env.COINMARKETCAP_API_KEY
    },
-   contractSizer:{
+   contractSizer: {
       runOnCompile: false,
       only: ["Lottery"]
    },
-   namedAccounts:{
-      deployer:{
+   namedAccounts: {
+      deployer: {
          default: 0,
          1: 0
       },
-      player:{
+      player: {
          default: 1
       }
    },
-   solidity:{
-      compilers:[
+   solidity: {
+      compilers: [
          {
-            version: "0.8.7"
+            version: "0.8.17"
          },
          {
             version: "0.4.24"
          },
       ]
    },
-   mocha:{
+   mocha: {
       timeout: 500000
    }
 }
