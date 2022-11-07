db99b820dce4ed8798b95454fc7ffdad3d3bfded
LaupWing <loc_strict@hotmail.com>
2022-11-07 16:05:43 +0100

    added test

--- lottery_smartcontract/test/staging/Lottery.staging.test.js
+++ lottery_smartcontract/test/staging/Lottery.staging.test.js
@@ -1,3 +1,4 @@
+const { expect } = require("chai");
 const { network, getNamedAccounts, ethers } = require("hardhat");
 const { developmentChains } = require("../../helper-hardhat-config");
 
@@ -13,7 +14,40 @@ developmentChains.includes(network.name)
          lotteryEntranceFee = await lottery.getEntranceFee()
       })
 
-      describe("End to End test", function () {
-         console.log(lottery)
+      describe("fulfillRandomWords", function () {
+         it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async () => {
+            console.log("Setting up test")
+            const startingTimestamp = await lottery.getLastTimestamp()
+            const accounts = await ethers.getSigners()
+
+            console.log("Setting up Listener...")
+            await new Promise(async (resolve, reject) => {
+               lottery.once("WinnerPicked", async () => {
+                  console.log("WinnerPicked event fired!")
+                  try {
+                     const recentWinner = await lottery.getRecentWinner()
+                     const lotteryState = await lottery.getLotteryState()
+                     const winnerEndingBalance = await accounts[0].getBalance()
+                     const endingTimestamp = await lottery.getLastTimestamp()
+
+                     await expect(lottery.getPlayer(0)).to.be.reverted
+                     expect(recentWinner.toString()).equal(accounts[0].address)
+                     expect(lotteryState, 0)
+                     expect(winnerEndingBalance.toString()).equal(winnerStartingBalance.add(lotteryEntranceFee).toString())
+                     expect(endingTimestamp).greaterThan(startingTimestamp)
+                     resolve()
+                  } catch (e) {
+                     console.log(e)
+                     reject(e)
+                  }
+               })
+
+               console.log("Enter Lottery...")
+               const tx = await lottery.enterLottery({ value: lotteryEntranceFee })
+               await tx.wait(1)
+               console.log("Ok, time to wait...")
+               const winnerStartingBalance = await accounts[0].getBalance()
+            })
+         })
       })
    })
