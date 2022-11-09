2ef78cc7c69bf83a15c433bb0bd20784ef7ee857
LaupWing <loc_strict@hotmail.com>
2022-11-09 15:47:15 +0100

    added all the events

--- vrf_example/contracts/RandomNumber.sol
+++ vrf_example/contracts/RandomNumber.sol
@@ -5,31 +5,9 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract Lock {
-   uint public unlockTime;
-   address payable public owner;
+contract RandomNumber {
+   event RequestEvent(uint256 requestId, uint32 numWords);
+   event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
-   event Withdrawal(uint amount, uint when);
-
-   constructor(uint _unlockTime) payable {
-      require(
-         block.timestamp < _unlockTime,
-         "Unlock time should be in the future"
-      );
-
-      unlockTime = _unlockTime;
-      owner = payable(msg.sender);
-   }
-
-   function withdraw() public {
-      // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-      // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-      require(block.timestamp >= unlockTime, "You can't withdraw yet");
-      require(msg.sender == owner, "You aren't the owner");
-
-      emit Withdrawal(address(this).balance, block.timestamp);
-
-      owner.transfer(address(this).balance);
-   }
+   
 }
