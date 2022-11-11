71061022da8395cf985355c9c3d830b94dec30d7
LaupWing <loc_strict@hotmail.com>
2022-11-11 16:22:17 +0100

    subscriptionID

--- vrf_example/deploy/01-deploy-random-words.js
+++ vrf_example/deploy/01-deploy-random-words.js
@@ -1,12 +1,20 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 const { DEVELOPMENT_CHAINS } = require("../helper-hardhat-config")
 
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
+   const { deploy, log } = deployments
    const { deployer } = await getNamedAccounts()
    const chainId = network.config.chainId
+   let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
 
    if (DEVELOPMENT_CHAINS.includes(network.name)) {
+      vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+      vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+
+      const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+      const transactionReceipt = await transactionResponse.wait()
+      subscriptionId = transactionReceipt.events[0].args.subId
 
    }
 }
