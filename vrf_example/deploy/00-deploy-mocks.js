a38b48331711e3acf48e00de6293a8148b6a3e90
LaupWing <loc_strict@hotmail.com>
2022-11-11 16:06:41 +0100

    added mock deploy scritps

---
+++ vrf_example/deploy/00-deploy-mocks.js
@@ -0,0 +1,10 @@
+
+const BASE_FEE = "250000000000000000"
+const GAS_PRICE_LINK = 1e9
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+   const { deploy, log } = deployments
+   const { deployer } = await getNamedAccounts()
+
+   // if(chainid ===)
+}
