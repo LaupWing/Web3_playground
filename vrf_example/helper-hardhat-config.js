221ddb96ef3cce700f877e58b590de3f8279b351
LaupWing <loc_strict@hotmail.com>
2022-11-11 16:01:23 +0100

    update

--- vrf_example/helper-hardhat-config.js
+++ vrf_example/helper-hardhat-config.js
@@ -6,12 +6,14 @@ const networkConfig = {
    31337: {
       name: "localhost",
       subscriptionId: "",
-      gasLane: ""
+      gasLane: "",
+      vrfCoordinatorV2: "",
    },
    5: {
       name: "goerli",
       subscriptionId: "",
-      gasLane: ""
+      gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+      vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D"
    }
 }
 
