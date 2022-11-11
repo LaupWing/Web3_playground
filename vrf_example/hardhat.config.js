74aebfe46aca07001fc058c0c0f8210c5885be2a
LaupWing <loc_strict@hotmail.com>
2022-11-11 15:55:37 +0100

    scan

--- vrf_example/hardhat.config.js
+++ vrf_example/hardhat.config.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -21,6 +22,15 @@ module.exports = {
       localhost: {
          chainId: 31337
       },
+      goerli: {
+         url: GOERLI_RPC_URL,
+         accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : []
+      }
+   },
+   etherscan: {
+      apiKey: {
+         goerli: ETHERSCAN_API_KEY
+      }
    },
    namedAccounts: {
       deployer: {
