d7e9646b8e5e7fe4b7dd985306f6b2d5237f997e
LaupWing <loc_strict@hotmail.com>
2022-11-12 16:00:51 +0100

    testing if it will host in goerli

--- vrf_example/helper-hardhat-config.js
+++ vrf_example/helper-hardhat-config.js
@@ -11,7 +11,7 @@ const networkConfig = {
    },
    5: {
       name: "goerli",
-      subscriptionId: "",
+      subscriptionId: "6223",
       gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
       vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D"
    }
