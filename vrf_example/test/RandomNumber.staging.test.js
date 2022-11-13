145e0d97b464c333fae8d1a3399a3296d07a4648
LaupWing <loc_strict@hotmail.com>
2022-11-13 09:05:25 +0100

    update

--- vrf_example/test/RandomNumber.staging.test.js
+++ vrf_example/test/RandomNumber.staging.test.js
@@ -8,9 +8,11 @@ DEVELOPMENT_CHAINS.includes(network.name)
       let randomNumberContract
 
       beforeEach(async () => {
-         randomNumberContract = await ethers.getContractAt("RandomNumber", "0x3fD2C9a79043b98F609C20F962D3FCE1Bc7c10B8")
-         const MAXIMUM = await randomNumberContract.getMaximum()
-         console.log(MAXIMUM.toString())
+         randomNumberContract = await ethers.getContractAt(
+            "RandomNumber",
+            "0x97CdcF86Dc624a1942D1729770e39c2DD23ce44e"
+         )
+         // const MAXIMUM = await randomNumberContract.getMaximum()
       })
       it("test", () => {
 
