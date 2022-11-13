2ca1c2c1100a520f01bbe85526bc827e49ac769d
LaupWing <loc_strict@hotmail.com>
2022-11-13 09:14:02 +0100

    gettign thecorrect user

--- vrf_example/test/RandomNumber.staging.test.js
+++ vrf_example/test/RandomNumber.staging.test.js
@@ -9,17 +9,18 @@ DEVELOPMENT_CHAINS.includes(network.name)
 
       beforeEach(async () => {
          deployer = (await getNamedAccounts()).deployer
-         randomNumberContract = await ethers.getContractAt(
-            "RandomNumber",
-            "0x97CdcF86Dc624a1942D1729770e39c2DD23ce44e",
-            deployer
-         )
+         console.log(deployer)
+         // randomNumberContract = await ethers.getContractAt(
+         //    "RandomNumber",
+         //    "0x97CdcF86Dc624a1942D1729770e39c2DD23ce44e",
+         //    deployer
+         // )
          // const MAXIMUM = await randomNumberContract.getMaximum()
       })
       it("test", async () => {
-         await new Promise(async (resolve, reject) => {
-            const transaction = await randomNumberContract.requestRandomNumber()
-            transaction.wait(1)
-         })
+         // await new Promise(async (resolve, reject) => {
+         //    const transaction = await randomNumberContract.requestRandomNumber()
+         //    transaction.wait(1)
+         // })
       })
    })
