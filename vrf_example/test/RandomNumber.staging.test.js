8e7a8a2824027d655b02eb74d096434cce1eaa12
LaupWing <loc_strict@hotmail.com>
2022-11-13 09:09:26 +0100

    deployer

--- vrf_example/test/RandomNumber.staging.test.js
+++ vrf_example/test/RandomNumber.staging.test.js
@@ -1,20 +1,25 @@
-const { network, ethers } = require("hardhat");
+const { network, ethers, getNamedAccounts } = require("hardhat");
 const { DEVELOPMENT_CHAINS } = require("../helper-hardhat-config");
 
 
 DEVELOPMENT_CHAINS.includes(network.name)
    ? describe.skip
    : describe("Random Number Staging test", function () {
-      let randomNumberContract
+      let randomNumberContract, deployer
 
       beforeEach(async () => {
+         deployer = (await getNamedAccounts()).deployer
          randomNumberContract = await ethers.getContractAt(
             "RandomNumber",
-            "0x97CdcF86Dc624a1942D1729770e39c2DD23ce44e"
+            "0x97CdcF86Dc624a1942D1729770e39c2DD23ce44e",
+            deployer
          )
          // const MAXIMUM = await randomNumberContract.getMaximum()
       })
-      it("test", () => {
-
+      it("test", async () => {
+         await new Promise(async (resolve, reject) => {
+            const transaction = await randomNumberContract.requestRandomNumber()
+            transaction.wait(1)
+         })
       })
    })
