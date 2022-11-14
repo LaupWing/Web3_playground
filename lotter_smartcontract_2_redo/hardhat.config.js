7222b8331a9fa15bc53747cf6e04e968a4a79534
LaupWing <loc_strict@hotmail.com>
2022-11-14 15:06:48 +0100

    base comments

--- lotter_smartcontract_2_redo/hardhat.config.js
+++ lotter_smartcontract_2_redo/hardhat.config.js
@@ -1,3 +1,11 @@
+// Hardhat toolbox imports the following
+// "@nomicfoundation/hardhat-chai-matchers"
+// "@nomiclabs/hardhat-ethers"
+// "@nomiclabs/hardhat-etherscan"
+// "@typechain/hardhat"
+// "hardhat-gas-reporter"
+// "solidity-coverage"
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
