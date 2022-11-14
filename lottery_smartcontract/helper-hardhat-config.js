1beb7409842108ea1a24a865621f7c668a51de36
LaupWing <loc_strict@hotmail.com>
2022-11-14 15:25:04 +0100

    update

--- lottery_smartcontract/helper-hardhat-config.js
+++ lottery_smartcontract/helper-hardhat-config.js
@@ -8,7 +8,7 @@ const networkConfig = {
    31337: {
       name: "localhost",
       subscriptionId: "6223",
-      gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+      gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
       keepersUpdateInterval: "30",
       lotteryEntranceFee: ethers.utils.parseEther("0.01"),
       callbackGasLimit: "500000"
@@ -16,7 +16,7 @@ const networkConfig = {
    5: {
       name: "goerli",
       subscriptionId: "6223",
-      gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+      gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
       lotteryEntranceFee: ethers.utils.parseEther("0.01"),
       keepersUpdateInterval: "30",
       callbackGasLimit: "500000",
