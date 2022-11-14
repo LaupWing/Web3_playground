fff7b25b70467cad1dd04cfc124654332db4f266
LaupWing <loc_strict@hotmail.com>
2022-11-14 15:15:06 +0100

    update yeah dit is nergens voor nodig

--- lotter_smartcontract_2_redo/contracts/Lock.sol
+++ lotter_smartcontract_2_redo/contracts/Lock.sol
@@ -1,34 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+import "hardhat/console.sol";
 
-    event Withdrawal(uint amount, uint when);
+contract Lottery {
+   enum LotteryState{
+      OPEN,
+      CALCULATING
+   }
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+   VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+   uint64 private immutable i_subscriptionId;
+   bytes32 private immutable i_gasLane;
+   uint32 private immutable i_callbackGasLimit;
+   uint16 private constant REQUEST_CONFIRMATIONS = 3;
+   uint32 private constant NUM_WORDS = 1;
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
 }
