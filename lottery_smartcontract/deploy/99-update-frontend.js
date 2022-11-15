ce053e9825fe4fa98fb166104b78355c99e001c9
LaupWing <loc_strict@hotmail.com>
2022-11-15 14:57:41 +0100

    updateAbi

--- lottery_smartcontract/deploy/99-update-frontend.js
+++ lottery_smartcontract/deploy/99-update-frontend.js
@@ -1,7 +1,20 @@
+const { ethers } = require("hardhat")
+const fs = require("fs")
+const { frontendAbiFile } = require("../helper-hardhat-config")
+
+const updateAbi = async () => {
+   const lottery = await ethers.getContract("Lottery")
+   fs.writeFileSync(frontendAbiFile, lottery.interface.format(ethers.utils.FormatTypes.json))
+}
+
+
+
 module.exports = async () => {
    if (process.env.UPDATE_FRONT_END) {
 
    }
 }
+
+
 
 module.exports.tags = ["all", "frontend"]
