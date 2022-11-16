430768cb8b4e520521807ad8b97a834e4d19bfc3
LaupWing <loc_strict@hotmail.com>
2022-11-16 14:36:18 +0100

    ai

--- lottery_frontend/components/LotteryEntrance.jsx
+++ lottery_frontend/components/LotteryEntrance.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import { useMoralis } from "react-moralis"
+import React, { useEffect, useState } from 'react'
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { useNotification } from "web3uikit"
+import { abi, contractAddresses } from "../constants"
 
 const LotteryEntrance = () => {
    const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis()
+   const chainId = parseInt(chainIdHex)
+   const lotteryAddress = chainId in contractAddresses
+      ? contractAddresses[chainId][0]
+      : null
 
+   const [entranceFee, setEntranceFee] = useState("0")
+   const [numberOfPlayers, setNumberOfPlayers] = useState("0")
+   const [recentWinner, setRecentWinner] = useState("0")
+
+   const dispatch = useNotification()
+
+
+   const { runContractFunction: getEntranceFee } = useWeb3Contract({
+      abi,
+      contractAddress: lotteryAddress,
+      functionName: "getEntranceFee",
+      params: {}
+   })
+
+   const updateUI = async () => {
+      const test = await getEntranceFee()
+      console.log(test.toString())
+   }
+
+   useEffect(() => {
+      updateUI()
+   }, [])
 
    return (
       <div>LotteryEntrance</div>
