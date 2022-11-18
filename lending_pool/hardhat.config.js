4f9465799a2b03cd4130825247231f13e330e8e8
LaupWing <loc_strict@hotmail.com>
2022-11-18 14:30:03 +0100

    IERc

--- lending_pool/hardhat.config.js
+++ lending_pool/hardhat.config.js
@@ -1,33 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
 require("dotenv").config()
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
+const MAINNET_RPC_URL =
+   process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
    solidity: {
       compilers: [
          {
-            version: "0.8.17 "
+            version: "0.8.17 ",
          },
          {
-            version: "0.4.19"
-         }
-      ]
+            version: "0.4.19",
+         },
+         {
+            version: "0.6.12",
+         },
+      ],
    },
    defaultNetwork: "hardhat",
    networks: {
       hardhat: {
          chainId: 31337,
          forking: {
-            url: MAINNET_RPC_URL
-         }
-      }
+            url: MAINNET_RPC_URL,
+         },
+      },
    },
    namedAccounts: {
       deployer: {
          default: 0,
-         1: 0
-      }
-   }
-};
+         1: 0,
+      },
+   },
+}
