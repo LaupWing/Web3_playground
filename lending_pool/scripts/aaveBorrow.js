5c179277543e80e103247b6e2c8258e7e0791ebd
LaupWing <loc_strict@hotmail.com>
2022-11-18 15:05:02 +0100

    udpate

--- lending_pool/scripts/aaveBorrow.js
+++ lending_pool/scripts/aaveBorrow.js
@@ -23,6 +23,17 @@ async function getLendingPool(account) {
    return lendingPool
 }
 
+async function approveErc20(spenderAddress, amount, signer) {
+   const erc20Token = await ethers.getContractAt(
+      "IERC20",
+      "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      signer
+   )
+   transaction_response = await erc20Token.approve(spenderAddress, amount)
+   await transaction_response.wait(1)
+   console.log("Approved")
+}
+
 main()
    .then(() => process.exit(0))
    .catch((err) => {
