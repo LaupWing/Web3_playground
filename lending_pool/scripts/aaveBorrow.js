0b4be6c66d805ced2eed06b1fc7e4bb48cd409a7
LaupWing <loc_strict@hotmail.com>
2022-11-18 14:49:50 +0100

    getLendingPool

--- lending_pool/scripts/aaveBorrow.js
+++ lending_pool/scripts/aaveBorrow.js
@@ -1,15 +1,16 @@
-const { ethers } = require("hardhat")
+const { ethers, getNamedAccounts } = require("hardhat")
 const { getWeth } = require("./getWeth")
 
 async function main() {
    await getWeth()
-   // const lendingPool = await
+   const { deployer } = await getNamedAccounts()
+   const lendingPool = await getLendingPool(deployer)
 }
 
 async function getLendingPool(account) {
    const lendingPoolAddressesProvider = await ethers.getContractAt(
       "ILendingPoolAddressesProvider",
-      "0x24a42fD28C976A61Df5D00D0599C34c4f90748c8",
+      "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
       account
    )
    const lendingPoolAddress =
