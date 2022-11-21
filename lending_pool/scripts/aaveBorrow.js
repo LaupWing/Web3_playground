dda8ca16ed546bf1deb3dbe59ca40d8412135d28
LaupWing <loc_strict@hotmail.com>
2022-11-21 15:12:56 +0100

    udpate

--- lending_pool/scripts/aaveBorrow.js
+++ lending_pool/scripts/aaveBorrow.js
@@ -2,6 +2,7 @@ const { ethers, getNamedAccounts } = require("hardhat")
 const { getWeth, AMOUNT } = require("./getWeth")
 
 const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
 
 async function getLendingPool(account) {
    const lendingPoolAddressesProvider = await ethers.getContractAt(
@@ -51,7 +52,7 @@ async function getBorrowUserData(lendingPool, account) {
 
 async function borrowDai(lendingPool, amountDaiToBorrow, account) {
    const borrow_transaction = await lendingPool.borrow(
-      "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+      DAI_ADDRESS,
       amountDaiToBorrow,
       1,
       0,
@@ -61,8 +62,16 @@ async function borrowDai(lendingPool, amountDaiToBorrow, account) {
    console.log("You've borrowed!")
 }
 
-async function repay(amount, account) {
-   await approveErc20("0x6B175474E89094C44Da98b954EedeAC495271d0F", amount)
+async function repay(amount, lendingPool, account) {
+   await approveErc20(DAI_ADDRESS, lendingPool.address, amount, account)
+   const repay_transaction = await lendingPool.repay(
+      DAI_ADDRESS,
+      amount,
+      1,
+      account
+   )
+   await repay_transaction.wait(1)
+   console.log("Repaid!")
 }
 
 async function main() {
@@ -93,6 +102,8 @@ async function main() {
    )
    console.log(`You can borrow ${amountDaiToBorrow.toString()} DAI`)
    await borrowDai(lendingPool, amountDaiToBorrowWei, deployer)
+   await getBorrowUserData(lendingPool, deployer)
+   await repay(amountDaiToBorrowWei, lendingPool, deployer)
    await getBorrowUserData(lendingPool, deployer)
 }
 
