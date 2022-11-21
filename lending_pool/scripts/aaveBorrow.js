3a61d5b6357552d8a6457328dc3728e7956990c7
LaupWing <loc_strict@hotmail.com>
2022-11-21 14:36:58 +0100

    update

--- lending_pool/scripts/aaveBorrow.js
+++ lending_pool/scripts/aaveBorrow.js
@@ -1,14 +1,6 @@
 const { ethers, getNamedAccounts } = require("hardhat")
 const { getWeth, AMOUNT } = require("./getWeth")
 
-async function main() {
-   await getWeth()
-   const { deployer } = await getNamedAccounts()
-   const lendingPool = await getLendingPool(deployer)
-   await approveErc20(lendingPool.address, AMOUNT, deployer)
-   console.log("Depositing")
-}
-
 async function getLendingPool(account) {
    const lendingPoolAddressesProvider = await ethers.getContractAt(
       "ILendingPoolAddressesProvider",
@@ -17,7 +9,7 @@ async function getLendingPool(account) {
    )
    const lendingPoolAddress =
       await lendingPoolAddressesProvider.getLendingPool()
-   const lendingPool = await ethers.getContract(
+   const lendingPool = await ethers.getContractAt(
       "ILendingPool",
       lendingPoolAddress,
       account
@@ -34,6 +26,33 @@ async function approveErc20(spenderAddress, amount, signer) {
    transaction_response = await erc20Token.approve(spenderAddress, amount)
    await transaction_response.wait(1)
    console.log("Approved")
+}
+
+async function getBorrowUserData(lendingPool, account) {
+   const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+      await lendingPool.getUserAccountData(account)
+
+   console.log(`You have ${totalCollateralETH} worth of ETH deposited`)
+   console.log(`You have ${totalDebtETH} worth of ETH borrowed`)
+   console.log(`You can borrow ${availableBorrowsETH} worth of ETH`)
+   return {
+      totalDebtETH,
+      availableBorrowsETH,
+   }
+}
+
+async function main() {
+   await getWeth()
+   const { deployer } = await getNamedAccounts()
+   const lendingPool = await getLendingPool(deployer)
+
+   await approveErc20(lendingPool.address, AMOUNT, deployer)
+   console.log("Depositing")
+
+   let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+      lendingPool,
+      deployer
+   )
 }
 
 main()
