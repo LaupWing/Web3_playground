387fd828e5b3255e1d9b0455d37bda566af0c0b2
LaupWing <loc_strict@hotmail.com>
2022-11-23 14:33:09 +0100

    imports

--- random_ipfs_nft/contracts/RandomIpfsNft.sol
+++ random_ipfs_nft/contracts/RandomIpfsNft.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
 contract RandomIpfsNft {
    
 }
