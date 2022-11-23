16af47e4cb3964aa184e21e91401f928010fc15d
LaupWing <loc_strict@hotmail.com>
2022-11-23 14:28:02 +0100

    helper hardhat config

--- random_ipfs_nft/hardhat.config.js
+++ random_ipfs_nft/hardhat.config.js
@@ -1,7 +1,45 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
 
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL =
+   process.env.GOERLI_RPC_URL ||
+   "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
    solidity: "0.8.17",
+   networks: {
+      hardhat: {
+         chainId: 31337,
+      },
+      goerli: {
+         url: GOERLI_RPC_URL,
+         accounts: [PRIVATE_KEY],
+         chainId: 5,
+         blockConfirmations: 6,
+      },
+      mainnet: {},
+   },
+   etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+   },
+   gasReporter: {
+      enabled: true,
+      currency: "USD",
+      outputFile: "gas-report.txt",
+      noColors: true,
+      coinmarketcap: COINMARKETCAP_API_KEY,
+   },
+   namedAccounts: {
+      deployer: {
+         default: 0,
+         1: 0,
+      },
+   },
+   mocha: {
+      timeout: 200000,
+   },
 }
