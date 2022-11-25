c5bbc28d678c14a0f3b95402314336af78fa4994
LaupWing <loc_strict@hotmail.com>
2022-11-25 14:48:30 +0100

    update

--- random_ipfs_nft/deploy/00-deploy-mocks.js
+++ random_ipfs_nft/deploy/00-deploy-mocks.js
@@ -1,13 +1,26 @@
 const { network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 
+const BASE_FEE = "250000000000000000"
+const GAS_PRICE_LINK = 1e9
 
 module.exports = async ({getNamedAccounts, deployments})=>{
    const { deploy, log } = deployments
    const { deployer } = await getNamedAccounts()
 
    if(developmentChains.includes(network.name)){
-      console.log("This is developmentchain")
+      console.log("Local network detected! Deploying mocks...")
+      await deploy("VRFCoordinatorV2Mock", {
+         from: deployer,
+         log: true,
+         args: [BASE_FEE, GAS_PRICE_LINK]
+      })
+
+      log("Mocks deployed!")
+      log("################################################################")
+      log("You are deploying to local network, you'll need a local network running to interact")
+      log("Please run `npx hardhat console --network localhost` to interact with the deployed contracts!!")
+      log("################################################################")
    }
 }
 
