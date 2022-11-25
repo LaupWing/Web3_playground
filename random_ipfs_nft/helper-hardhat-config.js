d96f1c8970cf1468ffed0d17627a1acc7cb65dae
LaupWing <loc_strict@hotmail.com>
2022-11-25 14:35:44 +0100

    update

--- random_ipfs_nft/helper-hardhat-config.js
+++ random_ipfs_nft/helper-hardhat-config.js
@@ -1,9 +1,28 @@
+const { ethers } = require("hardhat")
+
 const DECIMALS = "18"
 const INITIAL_PRICE = ""
 const developmentChains = ["hardhat", "localhost"]
 
+const networkConfig = {
+   31337: {
+      name: "localhost",
+      gasLane: "",
+      mintFee: ethers.utils.parseEther("0.01"),
+      callbackGasLimit: "500000" 
+   },
+   5: {
+      name: "goerli",
+      vrfCoordinatorV2: "",
+      gasLane: "",
+      callbackGasLimit: "500000",
+      subscriptionId: ""
+   }
+}
+
 module.exports = {
    developmentChains,
    DECIMALS,
-   INITIAL_PRICE
+   INITIAL_PRICE,
+   networkConfig
 }
