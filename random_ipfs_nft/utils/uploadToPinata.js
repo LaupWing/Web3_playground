9b5deaca5cd986364d6d8fb924560ad623b0be2d
LaupWing <loc_strict@hotmail.com>
2022-11-26 14:50:57 +0100

    update

--- random_ipfs_nft/utils/uploadToPinata.js
+++ random_ipfs_nft/utils/uploadToPinata.js
@@ -1,6 +1,7 @@
 const pinata_sdk = require("@pinata/sdk")
 const fs = require("fs")
 const path = require("path")
+const isImage = require("is-image")
 
 const pinata_api_key = process.env.PINATA_API_KEY || ""
 const pinata_api_secret = process.env.PINATA_API_SECRET || ""
@@ -9,7 +10,10 @@ const pinata = new pinata_sdk(pinata_api_key, pinata_api_secret)
 async function storeImages(images_file_path){
    const full_images_path = path.resolve(images_file_path)
    
-   const files = fs.readdirSync
+   const files = fs
+      .readdirSync(full_images_path)
+      .filter(file=> isImage(file))
+   console.log(files)
 }
 
 module.exports = {
