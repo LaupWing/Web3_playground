c46ba4b42893b0b5f29dffa6cd42f4d19091a081
LaupWing <loc_strict@hotmail.com>
2022-11-27 13:27:46 +0100

    udpate

---
+++ random_ipfs_nft/test/randomIpfs.test.js
@@ -0,0 +1,20 @@
+const { network, ethers, getNamedAccounts } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+
+
+!developmentChains.includes(network.name) 
+   ? describe.skip
+   : describe("Random IPFS NFT Unit Tests", function(){
+      let randomIpfsNft, deployer, vrfCoordinatorV2Mock
+
+      beforeEach(async ()=>{
+         deployer = (await getNamedAccounts()).deployer
+         
+      })
+
+      describe("Constructor", ()=>{
+         it("sets starting values correctly", async ()=>{
+
+         })
+      })
+   })
