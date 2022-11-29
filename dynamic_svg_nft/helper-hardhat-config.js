407b7a33c84db49e249ddaedad3a777f2cb07613
LaupWing <loc_strict@hotmail.com>
2022-11-29 14:48:36 +0100

    helper

---
+++ dynamic_svg_nft/helper-hardhat-config.js
@@ -0,0 +1,19 @@
+const networkConfig = {
+   31377: {
+      name: "localhost",
+      ethUsdPriceFeed: "",
+      callbackGasLimit: ""
+   },
+   5: {
+      name: "localhost",
+      ethUsdPriceFeed: "",
+      callbackGasLimit: ""
+   },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+   networkConfig,
+   developmentChains
+}
