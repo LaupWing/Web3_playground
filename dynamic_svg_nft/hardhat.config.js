741729133336a6d9d36577093ac390fa7a857818
LaupWing <loc_strict@hotmail.com>
2022-11-29 15:01:19 +0100

    update

--- dynamic_svg_nft/hardhat.config.js
+++ dynamic_svg_nft/hardhat.config.js
@@ -1,17 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const GOERLI_RPC_URL =
    process.env.GOERLI_RPC_URL ||
    "https://eth-goerli.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
    solidity: "0.8.17",
    defaultNetwork: "hardhat",
+   networks:{
+      hardhat: {
+         chainId: 31337
+      },
+      goerli: {
+         url: GOERLI_RPC_URL,
+         accounts: [PRIVATE_KEY],
+         chainId: 5,
+         blockConfirmations: 6,
+     },
+   },
    namedAccounts:{
       deployer: {
          default: 0,
