1f68e71f72a85a513af846a38b2bfae34f36e995
LaupWing <loc_strict@hotmail.com>
2022-12-02 15:05:28 +0100

    udpate

--- dynamic_svg_nft/deploy/01-deploy-dynamic-nft.js
+++ dynamic_svg_nft/deploy/01-deploy-dynamic-nft.js
@@ -1,6 +1,7 @@
 const { network } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const fs = require("fs")
+const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
    const { deploy, log } = deployments
@@ -23,5 +24,17 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
    log("--------------------------------------------------------")
    const args = [ethUsdPriceFeedAddress, lowSVG, highSVG]
-   
+   const dynamicSvgNft = await deploy("DynamicSvgNft", {
+      from: deployer,
+      args,
+      log: true,
+      waitConfirmations: network.config.blockConfirmations || 1
+   })
+
+   if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
+      log("Verifying...")
+      await verify(dynamicSvgNft.address, args)
+   }
 }
+
+module.exports.tags = ["all", "dynamicsvg"]
